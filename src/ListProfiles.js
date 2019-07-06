155619ab6f157505173d00b6a4d73253dba62336
Ericaemiliana <emilianapantoja90@gmail.com>
2019-07-06 18:29:59 +0000

    First commit

---
+++ src/ListProfiles.js
@@ -0,0 +1,17 @@
+/*import React, { Component } from 'react'
+class ListProfiles extends Component {
+  render(){
+    return (
+    <ol className='contact-list'>
+      {this.props.profiles.map((profile) => (
+    	<li key={profile.id} className='profile-list-item'>
+          <div 
+              className='profile-description'
+              <p>{`${profile.userID} s favorite movie is ${profile.favoriteMovieID}`}</p>
+			  <p>{`${userName}\'s favorite movie is "${favMovieName}."`}</p>
+          ></div>
+		</li>
+		))}
+		</ol>
+      )}}
+export default ListProfiles*/
